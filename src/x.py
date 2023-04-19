b26a296702c600aa1291b580b3406af4bd107254
Alessandro Carella <79840231+AlessandroCarella@users.noreply.github.com>
2023-04-20 00:56:48 +0200

    rimosso file caricato erroneamente

--- src/x.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-from os.path import join, isdir, isfile, abspath, dirname, splitext
-
-with open (join(abspath("src/analysis"), "video analysis only AUs in images format analysis.json"), "r") as f:
-    data = json.load (f)
-
-print (len (data))
