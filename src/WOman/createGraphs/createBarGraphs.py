0158fb40ce2acc7a963786f7f753f96080f72bdb
Alessandro Carella <79840231+AlessandroCarella@users.noreply.github.com>
2023-05-29 18:57:56 +0200

    aggiunti grafici bar e line per new transition e new task per tutte le classi di label

--- src/WOman/createGraphs/createBarGraphs.py
+++ src/WOman/createGraphs/createBarGraphs.py
@@ -1,8 +1,9 @@
 import re
 import matplotlib.pyplot as plt
-from os.path import join, dirname
+from os.path import join, dirname, abspath, splitext
+from os import listdir
 
-def barGraph (path):
+def createLineGraph (path, identifier):
     # Read the file and extract the relevant data
     with open(path, 'r') as file:
         content = file.read()
@@ -11,33 +12,71 @@ def barGraph (path):
     workflowName = re.search(r"New workflow : (.+)", content).group(1)
 
     # Extract the numbers of new tasks and new transitions for each case
-    newTasks = re.findall(r'New Tasks: (\.{0,})', content)
-    newTransitions = re.findall(r'NewTransitions: (\.{0,})', content)
+    data = re.findall(identifier + r': (\.{0,})', content)
 
     # Convert the extracted data to integer values
-    newTasks = [len(tasks) for tasks in newTasks]
-    newTransitions = [len(transitions) for transitions in newTransitions]
+    data = [len(tasks) for tasks in data]
 
     # Set the figure size to 1920x1080 pixels
     fig, ax = plt.subplots(figsize=(19.2, 10.8))
 
     # Create the bar graph
-    cases = range(1, len(newTasks) + 1)
+    cases = range(1, len(data) + 1)
     barWidth = 0.35
 
-    ax.bar(cases, newTasks, width=barWidth, label='New Tasks')
-    ax.bar([case + barWidth for case in cases], newTransitions, width=barWidth, label='New Transitions')
+    # Creazione del grafico a linee spezzate
+    plt.plot(cases, data, marker='o', linestyle='-', color='b')
+
+    # Personalizzazione del grafico
+    plt.title(workflowName + " " + identifier)
+    plt.xlabel("Cases")
+    plt.ylabel("Count")
+    
+    # Save the image in the same folder as the input file
+    plt.savefig(join(dirname(path), "graphs", "line graphs", workflowName + " " + identifier + '.png'), dpi=100)  # Set dpi to adjust resolution
+    plt.close()
+
+def createBarGraph (path, identifier):
+    # Read the file and extract the relevant data
+    with open(path, 'r') as file:
+        content = file.read()
+
+    # Extract the workflow name
+    workflowName = re.search(r"New workflow : (.+)", content).group(1)
+
+    # Extract the numbers of new tasks and new transitions for each case
+    data = re.findall(identifier + r': (\.{0,})', content)
+
+    # Convert the extracted data to integer values
+    data = [len(tasks) for tasks in data]
+
+    # Set the figure size to 1920x1080 pixels
+    fig, ax = plt.subplots(figsize=(19.2, 10.8))
+
+    # Create the bar graph
+    cases = range(1, len(data) + 1)
+    barWidth = 0.35
+
+    ax.bar(cases, data, width=barWidth, label=identifier)
 
     ax.set_xlabel('Cases')
     ax.set_ylabel('Count')
-    ax.set_title(workflowName)
+    ax.set_title(workflowName + " " + identifier)
     ax.legend()
 
     plt.tight_layout()
-
+    
     # Save the image in the same folder as the input file
-    plt.savefig(join(dirname(path), workflowName + '.png'), dpi=100)  # Set dpi to adjust resolution
+    plt.savefig(join(dirname(path), "graphs", "bar graphs", workflowName + " " + identifier + '.png'), dpi=100)  # Set dpi to adjust resolution
     plt.close()
 
-barGraph (r"C:\Users\Alessandro\Desktop\bachelor-s-thesis\src\richiesta professore\createGraphs\bored.out")
-barGraph (r"C:\Users\Alessandro\Desktop\bachelor-s-thesis\src\richiesta professore\createGraphs\confused.out")
+identifiers = [
+    "New Tasks",
+    "NewTransitions"
+]
+for file in listdir(dirname(abspath(__file__))):
+    if splitext(file)[1] == ".out":
+        outFile = join (dirname(abspath(__file__)), file)
+        for identifier in identifiers:
+            createBarGraph (outFile, identifier)
+            createLineGraph (outFile, identifier)
